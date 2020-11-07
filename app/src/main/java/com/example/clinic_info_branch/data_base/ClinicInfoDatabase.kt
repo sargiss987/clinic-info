53ff92dcc9c25358e796263f05844ac21ba0b7a3
sargi <sargiss@gmail.com>
2020-11-07 11:20:25 +0400

    change database

--- app/src/main/java/com/example/clinic_info_branch/data_base/ClinicInfoDatabase.kt
+++ app/src/main/java/com/example/clinic_info_branch/data_base/ClinicInfoDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.clinic_info_branch.data_base
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Notes::class,Patient::class], version = 1)
@@ -8,4 +10,26 @@ abstract class ClinicInfo : RoomDatabase() {
     abstract fun notesDao(): NotesDao
     abstract fun patientDao(): PatientDao
 
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: ClinicInfo? = null
+
+        fun getDatabase(context: Context): ClinicInfo {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ClinicInfo::class.java,
+                    "clinic_info"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
 }
